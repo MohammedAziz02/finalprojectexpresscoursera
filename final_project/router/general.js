14bb218f7440794a626ef6bfd73b215155cbeb3d
Mohammed Aziz <64214635+MohammedAziz02@users.noreply.github.com>
2023-03-02 13:29:29 +0100

    last push
    i have changed the general.js for the last tasks (promises)...

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -24,43 +24,64 @@ public_users.post("/register", (req, res) => {
 // Get the book list available in the shop
 public_users.get('/', function (req, res) {
   //Write your code here
-  return res.status(200).json(books);
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(res.status(200).json(books))
+    }, 1000)
+  })
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn', function (req, res) {
   const isbn = req.params.isbn;
-  for (let key in books) {
-    if (isbn == books[key].isbn) return res.status(200).json(books[key]);
-  }
-  return res.status(404).json({ message: `book with ISBN ${isbn} notfound` });
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      for (let key in books) {
+        if (isbn == books[key].isbn) resolve(res.status(200).json(books[key]));
+      }
+      reject(res.status(404).json({ message: `book with ISBN ${isbn} notfound` }));
+    }, 3000)
+
+  })
+
 });
 
 // Get book details based on author
 public_users.get('/author/:author', function (req, res) {
   const author = req.params.author;
-  for (let key in books) {
+  return new Promise(() => {
+    setTimeout(() => {
+      for (let key in books) {
 
-    if (author == books[key].author) return res.status(200).json(books[key]);
-  }
-  return res.status(404).json({ message: `book with author ${author} notfound` });
+        if (author == books[key].author) resolve(res.status(200).json(books[key]));
+      }
+      reject(res.status(404).json({ message: `book with author ${author} notfound` }));
+    }, 3000)
+  })
+
 });
 
 
 // Get all books based on title
 public_users.get('/title/:title', function (req, res) {
   const title = req.params.title;
-  for (let key in books) {
+  return new Promise(() => {
+    setTimeout(() => {
+      for (let key in books) {
 
-    if (title == books[key].title) return res.status(200).json(books[key]);
-  }
-  return res.status(404).json({ message: `book with title ${title} notfound` });
+        if (title == books[key].title) resolve(res.status(200).json(books[key]));
+      }
+      reject(res.status(404).json({ message: `book with title ${title} notfound` }));
+    }, 3000)
+  })
+
 });
 
 //  Get book review
 public_users.get('/review/:isbn', function (req, res) {
   //Write your code here
   const isbn = req.params.isbn;
+
   for (let key in books) {
     // search for the  book with the appropriate isbn
     if (isbn == books[key].isbn) {
